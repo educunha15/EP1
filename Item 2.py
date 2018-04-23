bfda706c78216324e7ff7b9c7b72f2fe16d4b760
Tiago Bergamo <tiago.niemeyer@hotmail.com>
2018-04-23 14:37:16 -0300

    Item 2 adcionado

---
+++ Item 2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 15 12:58:00 2018
+
+@author: tiago
+"""
+import json
+file = open("JSONTESTE.txt", "r")
+json_data = file.read()
+file.close()
+
+ESTOQUE = {}
+
+data_x = json.loads(json_data)
+
+
+while True:
+    a = int(input('''Controle de estoque-
+      0 - Sair
+      1 - Adicionar item
+      2 - Remover item
+      3 - Alterar item
+      4 - Imprimir estoque
+      Faça sua escolha:  '''))
+    if a == 0:
+        print ('Até mais!')
+        data_x = ESTOQUE
+        file = open("JSONTESTE.txt", "w")
+        file.write(json.dumps(data_x))
+        file.close()
+        break
+    elif a == 1:
+        while True:
+            b = input('Qual o nome do produto?:  ')
+            if b in ESTOQUE:
+                print ('Produto já está cadastrado')
+                break
+            else: 
+                ESTOQUE[b] = {}
+            while True:
+                c = int(input('Quantidade inicial:  '))
+                if c < 0:
+                    print ('A quantidade inicial não pode ser negativa')
+                else:
+                    ESTOQUE[b]['Quantidade'] = c
+                    break
+            break
+    elif a == 2:
+        d = input('Nome do produto:  ')
+        if d in ESTOQUE:
+            del ESTOQUE[d]
+            print ('Elemento excluido')
+        else:
+            print('Elemento não encontrado')
+    elif a == 3:
+        b = input('Nome do produto:  ')
+        if b in ESTOQUE:
+            e = int(input('Quantidade:  '))
+            ESTOQUE[b]['Quantidade'] += e
+            print('Novo estoque de {}: {}'.format(b,ESTOQUE[b]['Quantidade']))
+        else:
+            print ('Elemento não encontrado')
+    elif a == 4:
+        for i in ESTOQUE:
+            print(i,':',ESTOQUE[i]['Quantidade'])
+
